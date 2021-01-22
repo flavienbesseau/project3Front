702401d5a7640d16a6ef88b0245a9277b0e253c5
Alexandre-Dietsch <dietschalex@gmail.com>
2021-01-22 09:27:00 +0100

    dashboard

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,29 +1,37 @@
 import React, { useState } from "react";
-import logo from '../../assets/hospitalidee-logo.png';
+import logo from "../../assets/hospitalidee-logo.png";
 
 export default function Dashboard() {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
     <div className="dashboard">
-
       <div className="dashboard-header">
-        <div onClick={() => setIsOpen(!isOpen)} className="dashboard-header-menu" >
+        <div
+          onClick={() => setIsOpen(!isOpen)}
+          className="dashboard-header-menu"
+        >
           <span />
           <span />
           <span />
         </div>
-        <ul className={ isOpen ? 'routes-links-open' : 'routes-links' }>
-          <img src={logo} alt='' />
-          <li><a href='#'>Paramètres</a></li>
-          <li><a href='#'>Mon profil</a></li>
-          <li><a href='#'>Déconnexion</a></li>
+        <ul className={isOpen ? "routes-links-open" : "routes-links"}>
+          <img src={logo} alt="" />
+          <li>
+            <a href="#">Paramètres</a>
+          </li>
+          <li>
+            <a href="#">Mon profil</a>
+          </li>
+          <li>
+            <a href="#">Déconnexion</a>
+          </li>
         </ul>
-        <img src={logo} alt='' />
+        <img src={logo} alt="" />
       </div>
-      
+
       <div className="dashboard-sidebar">
-        <img src={logo} alt=''/>
+        <img src={logo} alt="" />
         <li>Tableau de bord</li>
         <li>Mon profil</li>
         <li>Enquêtes E-statis</li>
