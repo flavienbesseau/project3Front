6582f7bc8c8eb283eef57130e3e059bbfae4424c
Alexandre-Dietsch <dietschalex@gmail.com>
2021-01-28 21:05:55 +0100

    update radar margin

--- src/components/Charts/Repartition/RepartitionChart.jsx
+++ src/components/Charts/Repartition/RepartitionChart.jsx
@@ -47,7 +47,7 @@ export default function RepartitionChart() {
                 fontColor: "#fff",
               },
             },
-          }}  
+          }}
         />
       </div>
     </div>
